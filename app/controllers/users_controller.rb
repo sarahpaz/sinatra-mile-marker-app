c1edaa15174bc81b79e0775488f31ac861a70582
sarahpaz <sarah.paz@gmail.com>
2019-04-30 09:01:10 -0400

    add post signup conditional logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,15 @@ class UsersController < ApplicationController
     erb :'/users/signup'
   end
 
+  post '/users/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:run_goal].empty? || params[:password].empty?
+      redirect '/users/signup'
+    else
+      @user = User.new(params)
+      @user.save
+      session[:user_id] = @user.user_id
+      redirect "/users/#{@user.id}"
+    end
+  end
+
 end
