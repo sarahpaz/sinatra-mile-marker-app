225c67bdbd43acb79f5ee85a6b4087dd1068c21d
sarahpaz <sarah.paz@gmail.com>
2019-05-01 10:20:24 -0400

    add signup routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,53 @@
 class UsersController < ApplicationController 
+  get '/users/signup' do
+    if !logged_in?
+      erb :'/users/signup'  
+    else
+      redirect "/users/#{current_user.slug}" 
+    end
+  end
+
+  post '/users/signup' do
+    if params[:username] != "" || params[:email] != "" || params[:run_goal] != "" || params[:password] != ""
+      @user = User.create(username: params[:username], email: params[:email], run_goal: params[:run_goal], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.slug}"
+    else
+      erb :'/users/signup' # add error message about missing fields
+    end
+  end
 
   get '/users/login' do
-    
     if !logged_in?
       erb :'/welcome'
     else
-      redirect :"/users/#{@current_user.slug}"
+      redirect :"/users/#{current_user.slug}"
     end
   end
 
   post '/users/login' do
-    @user = User.find_by(params[:id])
-    
-    if @user.authenticate(params[:password])
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect "/users/#{@user.slug}"
-      erb :'/users/show'
     else
       redirect '/'
     end    
   end 
 
-  get '/users/:slug' do
-    @run = Run.all
-    erb :'/users/show'
+  get '/users/logout' do
+    if logged_in?
+      session.clear
+      redirect '/'
+    else
+      redirect '/'
+    end
   end
 
- 
+   get '/users/:slug' do
+    @run = Run.all
+    @user = User.find_by(username: params[:username])
+    erb :'/users/show'
+  end
 end
